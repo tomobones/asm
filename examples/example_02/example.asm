2c52726c4e7f9c76c6f840464d02b061a247dd6e
Thomas Vogg <thomas.vogg@posteo.de>
2026-03-15 23:53:58 +0100

    some changes

--- examples/example_02/example.asm
+++ examples/example_02/example.asm
@@ -8,7 +8,11 @@ global  _start	        ; linker needs this to find the entry point
 _start:
     mov rbp, rsp        ; save stack pointer for debugger
     nop
-; put your experiments between the two nop operations
+                        ; put your experiments between the two nop operations
     nop
+
+    mov rax,60          ; optional: code for exit syscall
+    mov rdi,0           ; optional: return a code of zero
+    syscall             ; optional: make kernel call
 
 section .bss            ; not strictly essential
